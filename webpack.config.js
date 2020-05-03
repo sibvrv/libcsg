feaf9a849766ba2d5fdf1a4488385f21dc6edd57
Roman Vereshchagin <info@sibvrv.com>
2020-05-03 13:19:35 +0700

    WebPack 4 initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+// Plugins
+const TSConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+// Variables
+const sourcePath = path.join(__dirname, './src/');
+const outPath = path.join(__dirname, `./dist/`);
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  return {
+    context: sourcePath,
+    entry: [
+      'index.ts'
+    ],
+    output: {
+      publicPath: '/',
+      library: 'libcsg',
+      libraryTarget: 'umd',
+      path: outPath,
+      filename: isProduction ? '[name].js' : '[name].js',
+      chunkFilename: isProduction ? 'chunks/[name].[contenthash].js' : 'chunks/[name].[hash].js'
+    },
+    target: 'node',
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+      plugins: [
+        new TSConfigPathsPlugin()
+      ],
+      mainFields: ['module', 'browser', 'main']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: ['ts-loader']
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin()
+    ],
+    devtool: isProduction ? 'hidden-source-map' : 'cheap-module-eval-source-map'
+  };
+};
