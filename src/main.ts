a49ad6d820f33302bf79e968ec3494f593b72e08
Roman Vereshchagin <info@sibvrv.com>
2020-05-03 22:51:25 +0700

    Update API entry point

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-export * from './api/index';
+import {circle, cube, cylinder, geodesicSphere, polygon, polyhedron, sphere, square, torus, triangle} from './primitives';
+
+export * as color from './color';
+
+export const primitives2d = {circle, square, polygon, triangle};
+export const primitives3d = {cube, sphere, geodesicSphere, cylinder, torus, polyhedron};
+
+export * as booleanOps from './modifiers/booleans';
+export * as transformations from './modifiers/transforms';
+export * as extrusions from './modifiers/extrusions';
+
+export const maths = require('./api/maths');
+export const text = require('./api/text');
+
+// these are 'external' to this api and we basically just re-export for old api compatibility
+// ...needs to be reviewed
+export const {CAG, CSG, isCAG, isCSG} = require('./csg');
