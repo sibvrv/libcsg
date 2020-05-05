09583305cec8f08e4f2cd4630115dc4b2076dc4e
Roman Vereshchagin <info@sibvrv.com>
2020-05-05 12:39:17 +0700

    Update helpers asserts test

--- test/helpers-asserts.spec.ts
+++ test/helpers-asserts.spec.ts
@@ -1,60 +1,62 @@
-import test from 'ava';
 import {comparePolygons} from './helpers/asserts';
+import {expect} from 'chai';
 
-test('comparePolygons on same single vertex', t => {
-  const a = {vertices: [{_x: 0, _y: 0, _z: 0}]};
-  t.true(comparePolygons(a, a));
-});
+describe('Helpers Asserts', () => {
+  it('comparePolygons on same single vertex', () => {
+    const a = {vertices: [{_x: 0, _y: 0, _z: 0}]};
+    expect(comparePolygons(a, a)).to.be.true;
+  });
 
-test('comparePolygons on different vertices', t => {
-  const a = {vertices: [{_x: 0, _y: 0, _z: 0}]};
-  const b = {vertices: [{_x: 1, _y: 1, _z: 1}]};
-  t.false(comparePolygons(a, b));
-});
+  it('comparePolygons on different vertices', () => {
+    const a = {vertices: [{_x: 0, _y: 0, _z: 0}]};
+    const b = {vertices: [{_x: 1, _y: 1, _z: 1}]};
+    expect(comparePolygons(a, b)).to.be.false;
+  });
 
-test('comparePolygons on same polygon', t => {
-  const a = {
-    vertices: [
-      {_x: 0, _y: 0, _z: 0},
-      {_x: 1, _y: 1, _z: 1},
-      {_x: -1, _y: 1, _z: 1},
-    ],
-  };
-  t.true(comparePolygons(a, a));
-});
+  it('comparePolygons on same polygon', () => {
+    const a = {
+      vertices: [
+        {_x: 0, _y: 0, _z: 0},
+        {_x: 1, _y: 1, _z: 1},
+        {_x: -1, _y: 1, _z: 1},
+      ],
+    };
+    expect(comparePolygons(a, a)).to.be.true;
+  });
 
-test('comparePolygons on same polygon with different vertice order', t => {
-  const a = {
-    vertices: [
-      {_x: 0, _y: 0, _z: 0},
-      {_x: 1, _y: 1, _z: 1},
-      {_x: -1, _y: 1, _z: 1},
-    ],
-  };
-  const b = {
-    vertices: [
-      {_x: -1, _y: 1, _z: 1},
-      {_x: 0, _y: 0, _z: 0},
-      {_x: 1, _y: 1, _z: 1},
-    ],
-  };
-  t.true(comparePolygons(a, b));
-});
+  it('comparePolygons on same polygon with different vertice order', () => {
+    const a = {
+      vertices: [
+        {_x: 0, _y: 0, _z: 0},
+        {_x: 1, _y: 1, _z: 1},
+        {_x: -1, _y: 1, _z: 1},
+      ],
+    };
+    const b = {
+      vertices: [
+        {_x: -1, _y: 1, _z: 1},
+        {_x: 0, _y: 0, _z: 0},
+        {_x: 1, _y: 1, _z: 1},
+      ],
+    };
+    expect(comparePolygons(a, b)).to.be.true;
+  });
 
-test('comparePolygons on different polygon with same vertice', t => {
-  const a = {
-    vertices: [
-      {_x: 0, _y: 0, _z: 0},
-      {_x: 1, _y: 1, _z: 1},
-      {_x: -1, _y: 1, _z: 1},
-    ],
-  };
-  const b = {
-    vertices: [
-      {_x: 0, _y: 0, _z: 0},
-      {_x: -1, _y: 1, _z: 1},
-      {_x: 1, _y: 1, _z: 1},
-    ],
-  };
-  t.false(comparePolygons(a, b));
+  it('comparePolygons on different polygon with same vertice', () => {
+    const a = {
+      vertices: [
+        {_x: 0, _y: 0, _z: 0},
+        {_x: 1, _y: 1, _z: 1},
+        {_x: -1, _y: 1, _z: 1},
+      ],
+    };
+    const b = {
+      vertices: [
+        {_x: 0, _y: 0, _z: 0},
+        {_x: -1, _y: 1, _z: 1},
+        {_x: 1, _y: 1, _z: 1},
+      ],
+    };
+    expect(comparePolygons(a, b)).to.be.false;
+  });
 });
