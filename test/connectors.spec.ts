41e02c2efd2155d8487075e23e57eeb8aafe9458
Roman Vereshchagin <info@sibvrv.com>
2020-05-05 12:52:44 +0700

    Update connectors test

--- test/connectors.spec.ts
+++ test/connectors.spec.ts
@@ -1,12 +1,16 @@
-import test from 'ava';
 import {CSG} from '../src/csg';
+import {expect} from 'chai';
 
-test('CSG.Connector exists', t => {
-  t.is('Connector' in CSG, true);
+describe('CSG Connectors', () => {
+  it('CSG.Connector exists', () => {
+    expect(CSG).to.have.own.property('Connector');
+  });
+
+  it('CSG.connectorslist can be instanciated', () => {
+    const observed = new CSG.ConnectorList();
+
+    expect(observed).to.have.own.property('connectors_');
+    expect(observed.connectors_).to.deep.eq([]);
+  });
 });
 
-test('CSG.connectorslist can be instanciated', t => {
-  const observed = new CSG.ConnectorList();
-
-  t.deepEqual(observed, {connectors_: []});
-});
