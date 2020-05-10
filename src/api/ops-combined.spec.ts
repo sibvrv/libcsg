4fe7bfa1a57b0dc86bfbc486a44c8c03490c7461
Roman Vereshchagin <info@sibvrv.com>
2020-05-10 10:37:31 +0700

    Update combined tests

--- src/api/ops-combined.spec.ts
+++ src/api/ops-combined.spec.ts
@@ -1,26 +1,31 @@
-import test from 'ava';
 import {circle, cube, torus} from '@root/primitives';
 import {linear_extrude} from '@modifiers/extrusions';
 import {intersection, union} from '@modifiers/booleans';
+import {expect} from 'chai';
 
 // any tests that involve multiple operands (extrude with union translate with difference etc)
 // and are not testing a specific feature (union, difference, translate etc)
 // belong here
+describe('Combined tests', () => {
+  describe('Extrude Tests', () => {
+    it('linear_extrude of union of 2d shapes', () => {
+      const obs = linear_extrude({height: 0.1}, union([
+        circle({r: 8, center: true}).translate([0, 20, 0]),
+        circle({r: 8, center: true}),
+      ]));
 
-test('linear_extrude of union of 2d shapes', t => {
-  const obs = linear_extrude({height: 0.1}, union([
-    circle({r: 8, center: true}).translate([0, 20, 0]),
-    circle({r: 8, center: true}),
-  ]));
+      expect(obs.polygons.length).to.eq(142);
+    });
+  });
 
-  t.deepEqual(obs.polygons.length, 142);
-});
+  describe('Intersection', () => {
+    it('intersection of torus where ro===r1 and cube', () => {
+      const obs = intersection(
+        torus({ro: 0.5, ri: 0.5}),
+        cube(),
+      );
 
-test('intersection of torus where ro===r1 and cube', t => {
-  const obs = intersection(
-    torus({ro: 0.5, ri: 0.5}),
-    cube(),
-  );
-
-  t.deepEqual(obs.polygons.length, 67);
+      expect(obs.polygons.length).to.eq(67);
+    });
+  });
 });
